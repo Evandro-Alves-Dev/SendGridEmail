57eba3f34176db638ad74ebd6ba818917b677d05
evandro.alves <evandroacer3@gmail.com>
2022-05-31 19:26:10 -0300

    Adicionado a dependencia lombok

--- src/main/java/com/casimiroti/integrations/dto/EmailDTO.java
+++ src/main/java/com/casimiroti/integrations/dto/EmailDTO.java
@@ -1,7 +1,14 @@
 package com.casimiroti.integrations.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmailDTO implements Serializable {
 
     private String fromEmail;
@@ -11,73 +18,4 @@ public class EmailDTO implements Serializable {
     private String subject;
     private String body;
     private String contentType;
-
-    public EmailDTO(){
-    }
-
-    public EmailDTO(String fromEmail, String fromName, String replyTo, String to, String subject, String body, String contentType) {
-        this.fromEmail = fromEmail;
-        this.fromName = fromName;
-        this.replyTo = replyTo;
-        this.to = to;
-        this.subject = subject;
-        this.body = body;
-        this.contentType = contentType;
-    }
-
-    public String getFromEmail() {
-        return fromEmail;
-    }
-
-    public void setFromEmail(String fromEmail) {
-        this.fromEmail = fromEmail;
-    }
-
-    public String getFromName() {
-        return fromName;
-    }
-
-    public void setFromName(String fromName) {
-        this.fromName = fromName;
-    }
-
-    public String getReplyTo() {
-        return replyTo;
-    }
-
-    public void setReplyTo(String replyTo) {
-        this.replyTo = replyTo;
-    }
-
-    public String getTo() {
-        return to;
-    }
-
-    public void setTo(String to) {
-        this.to = to;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public String getContentType() {
-        return contentType;
-    }
-
-    public void setContentType(String contentType) {
-        this.contentType = contentType;
-    }
 }
--- src/main/java/com/casimiroti/integrations/exceptions/handler/StandardError.java
+++ src/main/java/com/casimiroti/integrations/exceptions/handler/StandardError.java
@@ -1,7 +1,14 @@
 package com.casimiroti.integrations.exceptions.handler;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.Instant;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StandardError {
 
     private Instant timestamp;
@@ -9,47 +16,4 @@ public class StandardError {
     private String error;
     private String message;
     private String path;
-
-    public StandardError() {
-    }
-
-    public Instant getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(Instant timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public void setError(String error) {
-        this.error = error;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
 }
